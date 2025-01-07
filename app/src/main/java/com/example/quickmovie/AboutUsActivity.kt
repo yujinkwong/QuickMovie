5c1ee0df10c45febfc8e5cd2cff0858ea58cc318
Wong03 <choonyu03@gmail.com>
2025-01-08 00:55:02 +0800

    1

--- app/src/main/java/com/example/quickmovie/AboutUsActivity.kt
+++ app/src/main/java/com/example/quickmovie/AboutUsActivity.kt
@@ -4,12 +4,20 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 
 class AboutUsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.aboutus) // Loads the seat.xml layout
+        setContentView(R.layout.aboutus) // Loads the aboutus.xml layout
+
+        // Add click listener for the menu button
+        findViewById<ImageButton>(R.id.menubaraboutus).setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+            finish() // Close AboutUsActivity to avoid stacking multiple activities
+        }
     }
 
     // Method to open Facebook page when Facebook button is clicked
